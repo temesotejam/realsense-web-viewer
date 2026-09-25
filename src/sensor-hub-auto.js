78a45803205a52e948ad3739d91f7c654d524955
AIAAOOAO <159702851+temesotejam@users.noreply.github.com>
2026-09-25 19:12:27 +0900

    Align headless Hub API import to orientation build 20260925.7

--- src/sensor-hub-auto.js
+++ src/sensor-hub-auto.js
@@ -1,4 +1,4 @@
-import { sensorHub } from "./realsense-sensor-api.js?v=20260925.6";
+import { sensorHub } from "./realsense-sensor-api.js?v=20260925.7";
 
 const $ = (id) => document.getElementById(id);
 const state = {
